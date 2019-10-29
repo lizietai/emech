e97b82e268e5f301e3b52d95d5f62139efa792c6
lizietai <taiojuolapee@gmail.com>
2019-10-29 09:30:20 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,352 @@
+
+var $ = jQuery.noConflict();
+
+
+// Page Loader
+$(window).load(function () {
+    
+    "use strict";
+	$('#loader').fadeOut();
+});
+
+
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function () {
+    
+    "use strict";
+    
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 68
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top',
+	offset: 70
+	
+});
+
+
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function () {
+    "use strict";
+    $('.navbar-toggle:visible').click();
+});
+   
+/*----------------------------------------------------*/
+    /*	Account Modal
+    /*----------------------------------------------------*/
+    
+    $('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+    
+    /*----------------------------------------------------*/
+    /*	Css3 Transition
+    /*----------------------------------------------------*/
+
+        $('*').each(function(){
+            if($(this).attr('data-animation')) {
+                var $animationName = $(this).attr('data-animation'),
+                    $animationDelay = "delay-"+$(this).attr('data-animation-delay');
+                $(this).appear (function() {
+                    $(this).addClass('animated').addClass($animationName);
+                    $(this).addClass('animated').addClass($animationDelay);
+                });
+            }
+        });
+    
+    
+
+    
+    /*--------------------------------------------------*/
+    /* Counter*/
+    /*--------------------------------------------------*/ 
+        
+
+                $('.timer').countTo();
+
+                $('.counter-item').appear(function() {
+                    $('.timer').countTo();
+                },{accY: -100});
+            
+        
+
+
+
+////------- Testimonials Carousel
+	
+var testimonial = $("#testimonial-carousel");
+
+testimonial.owlCarousel({
+	navigation : true,
+	pagination: true,
+	slideSpeed : 2500,
+	stopOnHover: true,
+	autoPlay: 3000,
+	singleItem: true,
+	transitionStyle : "fade",
+	navigationText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+});
+
+	
+
+////------- Pricing Table
+
+$(document).ready(function() {
+ 
+  $(".pricing").owlCarousel({
+        pagination: true,
+        navigation : false,
+        slideSpeed : 2500,
+		stopOnHover: true,
+    	autoPlay: 3000,
+    	singleItem:false,
+        itemsMobile : [550,1],
+        itemsDesktopSmall : [991,2],
+        items: 4,
+		transitionStyle : "fade",
+		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+  });
+ 
+});
+
+
+////------- Latest News
+
+$(document).ready(function() {
+ 
+  $(".latest-news").owlCarousel({
+        pagination: true,
+        navigation : false,
+        slideSpeed : 2500,
+		stopOnHover: true,
+    	autoPlay: 4000,
+    	singleItem:false,
+        itemsMobile : [550,1],
+        itemsDesktopSmall : [991,2],
+        items: 3,
+		transitionStyle : "fade",
+		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+  });
+ 
+});
+
+
+
+
+// Team Carousel
+
+$(document).ready(function() {
+ 
+  $(".our-team").owlCarousel({
+        pagination: true,
+        slideSpeed : 2500,
+		stopOnHover: true,
+    	autoPlay: 3000,
+        items: 4,
+    	//singleItem:true,
+        itemsMobile : [550,1],
+        itemsDesktopSmall : [991,2],
+		transitionStyle : "fade",
+		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+  });
+ 
+});
+
+
+
+////------- Partner/CLients
+
+$(document).ready(function() {
+ 
+  $(".clients").owlCarousel({
+        pagination: false,
+        navigation : true,
+        slideSpeed : 2500,
+		stopOnHover: true,
+    	autoPlay: 3000,
+    	singleItem:false,
+        itemsMobile : [550,1],
+        itemsDesktopSmall : [991,2],
+        items: 5,
+		transitionStyle : "fade",
+		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+  });
+ 
+});
+
+
+
+
+$ ( function () {
+
+	$(".video").fitVids();
+	
+});
+
+
+
+
+
+        // Tab------------------------------
+
+
+$('#team a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+
+
+
+/*---------------------------------------------------*/
+    /* Progress Bar
+    /*---------------------------------------------------*/
+    $(document).ready(function($) {
+	"use strict";
+    
+        $('.skill-shortcode').appear(function() {
+            $('.progress').each(function(){ 
+                $('.progress-bar').css('width',  function(){ return ($(this).attr('data-percentage')+'%')});
+            });
+        },{accY: -100});
+        
+        
+    });
+
+$(document).ready( function() {
+    	$(document).on('change', '.btn-file :file', function() {
+		var input = $(this),
+			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		input.trigger('fileselect', [label]);
+		});
+
+		$('.btn-file :file').on('fileselect', function(event, label) {
+		    
+		    var input = $(this).parents('.input-group').find(':text'),
+		        log = label;
+		    
+		    if( input.length ) {
+		        input.val(log);
+		    } else {
+		        if( log ) alert(log);
+		    }
+	    
+		});
+		function readURL(input) {
+		    if (input.files && input.files[0]) {
+		        var reader = new FileReader();
+		        
+		        reader.onload = function (e) {
+		            $('#img-upload').attr('src', e.target.result);
+		        }
+		        
+		        reader.readAsDataURL(input.files[0]);
+		    }
+		}
+    
+
+		$("#imgInp").change(function(){
+		    readURL(this);
+		}
+                           
+                           ); 	
+	});
+
+	$(document).ready( function() {
+    	$(document).on('change', '.btn-file :file', function() {
+		var input = $(this),
+			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		input.trigger('fileselect', [label]);
+		});
+
+		$('.btn-file :file').on('fileselect', function(event, label) {
+		    
+		    var input = $(this).parents('.input-group').find(':text'),
+		        log = label;
+		    
+		    if( input.length ) {
+		        input.val(log);
+		    } else {
+		        if( log ) alert(log);
+		    }
+	    
+		});
+		function readURL(input) {
+		    if (input.files && input.files[0]) {
+		        var reader = new FileReader();
+		        
+		        reader.onload = function (e) {
+		            $('#img-upload1').attr('src', e.target.result);
+		        }
+		        
+		        reader.readAsDataURL(input.files[0]);
+		    }
+		}
+    
+
+		$("#imgInp1").change(function(){
+		    readURL(this);
+		}
+                           
+                           ); 	
+	});
+
+$(document).ready(function() {
+    document.getElementById('pro-image').addEventListener('change', readImage, false);
+    
+    $( ".preview-images-zone" ).sortable();
+    
+    $(document).on('click', '.image-cancel', function() {
+        let no = $(this).data('no');
+        $(".preview-image.preview-show-"+no).remove();
+    });
+});
+
+
+
+var num = 4;
+function readImage() {
+    if (window.File && window.FileList && window.FileReader) {
+        var files = event.target.files; //FileList object
+        var output = $(".preview-images-zone");
+
+        for (let i = 0; i < files.length; i++) {
+            var file = files[i];
+            if (!file.type.match('image')) continue;
+            
+            var picReader = new FileReader();
+            
+            picReader.addEventListener('load', function (event) {
+                var picFile = event.target;
+                var html =  '<div class="preview-image preview-show-' + num + '">' +
+                            '<div class="image-cancel" data-no="' + num + '">x</div>' +
+                            '<div class="image-zone"><img id="pro-img-' + num + '" src="' + picFile.result + '"></div>' +
+                            '<div class="tools-edit-image"><a href="javascript:void(0)" data-no="' + num + '" class="btn btn-light btn-edit-image">edit</a></div>' +
+                            '</div>';
+
+                output.append(html);
+                num = num + 1;
+            });
+
+            picReader.readAsDataURL(file);
+        }
+        $("#pro-image").val('');
+    } else {
+        console.log('Browser not support');
+    }
+}
+
+
+	
+
